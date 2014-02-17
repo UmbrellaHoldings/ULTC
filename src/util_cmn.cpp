b2e558e57cfef7b3ecf152e2925404993c75a21f
Sergei Lodyagin <serg@kogorta.dp.ua>
2014-02-18 01:09:11 +0200

    The USDollarCoinKeyGen utility is added
    Conflicts:
    	.gitignore
    
    Conflicts:
    	.gitignore

---
+++ src/util_cmn.cpp
@@ -0,0 +1,60 @@
+/* -*-coding: mule-utf-8-unix; fill-column: 58; -*-
+
+  Move some function from util.cpp here for use from cmd
+  line utils (util.cpp is mixed with Qt stuff)
+*/
+
+#include <openssl/rand.h>
+#include "util.h"
+
+void RandAddSeed()
+{
+    // Seed with CPU performance counter
+    int64 nCounter = GetPerformanceCounter();
+    RAND_add(&nCounter, sizeof(nCounter), 1.5);
+    memset(&nCounter, 0, sizeof(nCounter));
+}
+
+void RandAddSeedPerfmon()
+{
+    RandAddSeed();
+
+    // This can take up to 2 seconds, so only do it every 10 minutes
+    static int64 nLastPerfmon;
+    if (GetTime() < nLastPerfmon + 10 * 60)
+        return;
+    nLastPerfmon = GetTime();
+
+#ifdef WIN32
+    // Don't need this on Linux, OpenSSL automatically uses /dev/urandom
+    // Seed with the entire set of perfmon data
+    unsigned char pdata[250000];
+    memset(pdata, 0, sizeof(pdata));
+    unsigned long nSize = sizeof(pdata);
+    long ret = RegQueryValueExA(HKEY_PERFORMANCE_DATA, "Global", NULL, NULL, pdata, &nSize);
+    RegCloseKey(HKEY_PERFORMANCE_DATA);
+    if (ret == ERROR_SUCCESS)
+    {
+        RAND_add(pdata, nSize, nSize/100.0);
+        OPENSSL_cleanse(pdata, nSize);
+        printf("RandAddSeed() %lu bytes\n", nSize);
+    }
+#endif
+}
+
+//
+// "Never go to sea with two chronometers; take one or three."
+// Our three time sources are:
+//  - System clock
+//  - Median of other nodes clocks
+//  - The user (asking the user to fix the system clock if the first two disagree)
+//
+static int64 nMockTime = 0;  // For unit testing
+
+int64 GetTime()
+{
+    if (nMockTime) return nMockTime;
+
+    return time(NULL);
+}
+
