578a024cb75559974eab1b755d627ad0164d8252
Sergei Lodyagin <serg@kogorta.dp.ua>
2014-05-10 16:29:04 +0300

    UT execution is restored

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -29,14 +29,12 @@ struct TestingSetup {
         pblocktree = new CBlockTreeDB(1 << 20, true);
         pcoinsdbview = new CCoinsViewDB(1 << 23, true);
         pcoinsTip = new CCoinsViewCache(*pcoinsdbview);
-#if 0
         InitBlockIndex();
         bool fFirstRun;
         pwalletMain = new CWallet("wallet.dat");
         pwalletMain->LoadWallet(fFirstRun);
         RegisterWallet(pwalletMain);
         nScriptCheckThreads = 3;
-#endif
         for (int i=0; i < nScriptCheckThreads-1; i++)
             threadGroup.create_thread(&ThreadScriptCheck);
     }
