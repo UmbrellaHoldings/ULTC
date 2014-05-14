af8fee30a53843dc3fd403e76a786dfabe382ede
Maria Maximova <mary.maximova@gmail.com>
2014-05-14 22:05:40 +0300

    change port numbers to unique values 20141305143[8901]

--- contrib/wallettools/walletunlock.py
+++ contrib/wallettools/walletunlock.py
@@ -1,4 +1,4 @@
 from jsonrpc import ServiceProxy
-access = ServiceProxy("http://127.0.0.1:5888")
+access = ServiceProxy("http://127.0.0.1:201413051430")
 pwd = raw_input("Enter wallet passphrase: ")
 access.walletpassphrase(pwd, 60)
