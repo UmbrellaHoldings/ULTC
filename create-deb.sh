88981d350046a4e22b8e66fff25a18e2d4d0fb6c
barjomet <barjomet@ubuntu-wine-serg.kiddy.kogorta.dp.ua>
2014-06-05 13:39:07 +0300

    create-deb.sh modified and unified

--- create-deb.sh
+++ create-deb.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
+#example VERSION=1.2.3.4 NAME=bobcoin ./create-deb.sh /path/to/bobcoin-Qt
  
 CreateDeb_ROOT=$PWD/build/deb
 CreateDeb_OUT=$PWD/build/out
 CreateDeb_DEBIAN=$CreateDeb_ROOT/DEBIAN
 CreateDeb_CONTROL=$CreateDeb_DEBIAN/control
-CreateDeb_DEPS=$(ldd $@rhoscoin-qt | while read line; do pkg=$( echo "$line" |cut -f 1 -d " "); if [ $pkg != 'linux-vdso.so.1' ]; then echo $(dpkg -S $pkg| head -n1|cut -f 1 -d ":") \(\>= $(dpkg -s $(dpkg -S $pkg| head -n1|cut -f 1 -d ":") |grep 'Version'|cut -f 2 -d " "|cut -f 1 -d "-")\), ; fi; done |sort -u |tr -s '\r\n' ' '|sed 's/..$//') 
+CreateDeb_DEPS=$(ldd $@$NAME-qt | while read line; do pkg=$( echo "$line" |cut -f 1 -d " "); if [ $pkg != 'linux-vdso.so.1' ]; then echo $(dpkg -S $pkg| head -n1|cut -f 1 -d ":") \(\>= $(dpkg -s $(dpkg -S $pkg| head -n1|cut -f 1 -d ":") |grep 'Version'|cut -f 2 -d " "|cut -f 1 -d "-")\), ; fi; done |sort -u |tr -s '\r\n' ' '|sed 's/..$//') 
  
  
 rm -rf $CreateDeb_ROOT
@@ -15,18 +17,19 @@ if [ -e $VERSION ]; then
 	version=`date +%s`
 fi
  
-echo "Package: rhoscoin
+echo "Package: $NAME
 Version: $VERSION
 Architecture: amd64
 Depends: $CreateDeb_DEPS
 Maintainer: Sergei Lodyagin <serg@kogorta.dp.ua>
-Description: RHOS coin
+Description: ${NAME^}
  Peer-to-peer cryptocurrency" > $CreateDeb_CONTROL || exit 1
  
 
 mkdir -p $CreateDeb_ROOT/usr/bin || exit 1
 cp $@* $CreateDeb_ROOT/usr/bin || exit 1
  
-deb_name='rhoscoin_'$VERSION'.deb'
+deb_name=$NAME'_'$VERSION'.deb'
+mkdir -p $CreateDeb_OUT  || exit 1 
  
 dpkg -b $CreateDeb_ROOT $CreateDeb_OUT/$deb_name
