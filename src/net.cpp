02e97d9ab561ee6c3f69bf0cf40d6935c68131cf
Sergei Lodyagin <serg@kogorta.dp.ua>
2014-07-04 01:33:18 +0300

    DNS seeds added.

--- src/net.cpp
+++ src/net.cpp
@@ -1186,10 +1186,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
+    {"www.ltc.umbrella.moneta.cohors.bz", "ltc.umbrella.moneta.cohors.bz" },
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
+    {"www.ltc.umbrella.moneta.cohors.bz", "ltc.umbrella.moneta.cohors.bz" },
     {NULL, NULL}
 };
 
